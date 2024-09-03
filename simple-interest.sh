9440f525807aa23de74f4eef726062dddccd5de0
David <gerson2050@hotmail.com>
2024-09-03 12:30:58 -0500

    Corregido el cálculo de interés simple usando bc

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script calculates simple interest given principal, annual rate of interest and time period in years.
+# This script calculates simple interest given principal, annual rate of interest, and time period in years.
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
@@ -12,15 +12,14 @@
 # r, annual rate of interest
 
 # Output:
-# simple interest = p*t*r
+# simple interest = p*t*r/100
 
 echo "Enter the principal:"
 read p
-echo "Enter rate of interest per year:"
+echo "Enter rate of interest per year (in %):"
 read r
 echo "Enter time period in years:"
 read t
 
-s=$(expr $p \* $t \* $r / 100)
-echo "The simple interest is: "
-echo $s
+s=$(echo "scale=2; $p * $t * $r / 100" | bc)
+echo "The simple interest is: $s"
